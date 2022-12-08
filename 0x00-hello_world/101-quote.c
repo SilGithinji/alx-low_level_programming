0e8fd7766ceeefe90567a1ef0a08cba774d30079
SilGithinji <isaacgithinji4612@gmail.com>
2022-12-08 12:43:16 -0800

    My best commit. Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  * Description: 'print text followed by a new line'
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-return (0);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 59);
+return (1);
 }
