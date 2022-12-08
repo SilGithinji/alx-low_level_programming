934f3c808050ac642bf7c2fb37d515517b16cdf6
SilGithinji <isaacgithinji4612@gmail.com>
2022-12-08 02:27:05 -0800

    My best commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,6 +8,7 @@
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,");
+	int getchar(void);
+	return (0);
 }
