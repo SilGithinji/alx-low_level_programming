07cca62b3d3ad35b7bc04774290281caabf954ed
SilGithinji <isaacgithinji4612@gmail.com>
2022-12-08 02:15:49 -0800

    My best commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * 
+ * Description: 'print text and start new line'
+ * 
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
