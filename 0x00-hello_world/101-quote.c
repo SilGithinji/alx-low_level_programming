5f0ab36b3ad062705606bc7e8bb4f3eff053d691
SilGithinji <isaacgithinji4612@gmail.com>
2022-12-08 12:37:24 -0800

    My best commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  * Description: 'print text followed by a new line'
@@ -6,6 +7,6 @@
  */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 59);
 return (0);
 }
