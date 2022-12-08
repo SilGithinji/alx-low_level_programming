2055f9d8bc210ae1772c6e55d27b7f7e1357594e
SilGithinji <isaacgithinji4612@gmail.com>
2022-12-08 01:52:27 -0800

    My best commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line" '
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
