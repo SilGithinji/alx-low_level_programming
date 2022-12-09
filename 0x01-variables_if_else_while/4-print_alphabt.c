da3c330d0b3f31d5baead0914c507caea203bfe9
SilGithinji <isaacgithinji4612@gmail.com>
2022-12-09 06:11:05 -0800

    My best commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'print alphabets in lower case but q and e'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i <= 26; i++)
+	{
+		if (i != 5 && i != 17)
+			putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
