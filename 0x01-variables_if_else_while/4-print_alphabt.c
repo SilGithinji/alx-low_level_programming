0574dfef05f370f14d0e98d2990abae5b5ad0a55
SilGithinji <isaacgithinji4612@gmail.com>
2022-12-09 06:13:07 -0800

    My best commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,9 +9,9 @@ int main(void)
 	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
-	for (i = 0; i <= 26; i++)
+	for (i = 0; i < 26; i++)
 	{
-		if (i != 5 && i != 17)
+		if (i != 4 && i != 18)
 			putchar(alp[i]);
 	}
 	putchar('\n');
