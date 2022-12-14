83336041ac5250b3efa1676689855dcac18e5a88
SilGithinji <isaacgithinji4612@gmail.com>
2022-12-14 00:16:44 -0800

    My best commit. Task 0

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: character to print
+ *
+ * Return: on success 1
+ *
+ */
+int _putchar (char c)
+{
+	return (write(1, &c, 1));
+}
