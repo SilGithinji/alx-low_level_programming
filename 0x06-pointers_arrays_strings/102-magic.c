6084de1257fe10658127c19ba02542ea00756d37
SilGithinji <isaacgithinji4612@gmail.com>
2022-12-23 03:49:21 -0800

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
+
 int main(void)
 {
 	int vu;
-	int pbc[2];
-	int *p;
+	int pbc[5];
+	int *li;
 
 	pbc[2] = 1024;
-	p = &vu;
+	li = &vu;
 	/*
-	 * write your line of code here
-	 * Remember:
-	 * - you are not allowed to use a
-	 * - you are not allowed to modify p
-	 * - only one statement
-	 * - you are not allowed to code anything else than this line of code
-	 */
-	*(p + 5) = 98;
+ * write your line of code here
+ * Remember:
+ * - you are not allowed to use a
+ * - you are not allowed to modify p
+ * - only one statement
+ * - you are not allowed to code anything else than this line of code
+ */
+*(li + 5) = 98;
 	/* ... so that this prints 98\n */
 	printf("pbc[2] = %d\n", pbc[2]);
 	return (0);
