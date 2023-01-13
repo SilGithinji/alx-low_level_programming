9306d2f81cf5c9e331fd1a5dd08ba46011b16868
SilGithinji <isaacgithinji4612@gmail.com>
2023-01-13 14:06:19 -0800

    C - More malloc, free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -22,7 +22,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
-	if (!pi)
+	if (!ptr)
 		return (malloc(new_size));
 	pi = malloc(new_size);
 	if (!pi)
