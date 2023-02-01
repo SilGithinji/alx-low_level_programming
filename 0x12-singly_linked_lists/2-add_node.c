909bea1bc8430e49b42a003236a55e00827c318f
SilGithinji <isaacgithinji4612@gmail.com>
2023-02-01 01:14:20 -0800

    C - Singly linked lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,20 +11,20 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *n;
-	unsigned int l = 0;
+	list_t *new;
+	unsigned int len = 0;
 
-	while (str[l])
-		l++;
+	while (str[len])
+		len++;
 
-	n = malloc(sizeof(list_t));
-	if (!n)
+	new = malloc(sizeof(list_t));
+	if (!new)
 		return (NULL);
 
-	n->str = strdup(str);
-	n->l = l;
-	n->next = (*head);
-	(*head) = n;
+	new->str = strdup(str);
+	new->len = len;
+	new->next = (*head);
+	(*head) = new;
 
 	return (*head);
 }
